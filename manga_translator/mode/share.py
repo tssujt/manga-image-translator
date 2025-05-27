7e41e65580050e1d3482bcf1a25f08625fc29466
tssujt <github@tssujt.xyz>
2025-05-27 15:25:06 +0800

    Refactor MangaShare class by removing lock

--- manga_translator/mode/share.py
+++ manga_translator/mode/share.py
@@ -1,13 +1,10 @@
 import asyncio
 import pickle
-from threading import Lock
 
 import uvicorn
 from fastapi import FastAPI, HTTPException, Path, Request, Response
 from pydantic import BaseModel
 import inspect
-
-from starlette.responses import StreamingResponse
 
 from manga_translator import MangaTranslator
 
@@ -35,55 +32,11 @@ class MangaShare:
         self.port = int(params.get('port', '5003'))
         self.nonce = params.get('nonce', None)
 
-        # each chunk has a structure like this status_code(int/1byte),len(int/4bytes),bytechunk
-        # status codes are 0 for result, 1 for progress report, 2 for error
-        self.progress_queue = asyncio.Queue()
-        self.lock = Lock()
-
-        async def hook(state: str, finished: bool):
-            state_data = state.encode("utf-8")
-            progress_data = b'\x01' + len(state_data).to_bytes(4, 'big') + state_data
-            await self.progress_queue.put(progress_data)
-            await asyncio.sleep(0)
-
-        self.manga.add_progress_hook(hook)
-
-    async def progress_stream(self):
-        """
-        loops until the status is != 1 which is eiter an error or the result
-        """
-        while True:
-            progress = await self.progress_queue.get()
-            yield progress
-            if progress[0] != 1:
-                break
-
-    async def run_method(self, method, **attributes):
-        try:
-            if asyncio.iscoroutinefunction(method):
-                result = await method(**attributes)
-            else:
-                result = method(**attributes)
-            result_bytes = pickle.dumps(result)
-            encoded_result = b'\x00' + len(result_bytes).to_bytes(4, 'big') + result_bytes
-            await self.progress_queue.put(encoded_result)
-        except Exception as e:
-            err_bytes = str(e).encode("utf-8")
-            encoded_result = b'\x02' + len(err_bytes).to_bytes(4, 'big') + err_bytes
-            await self.progress_queue.put(encoded_result)
-        finally:
-            self.lock.release()
-
-
     def check_nonce(self, request: Request):
         if self.nonce:
             nonce = request.headers.get('X-Nonce')
             if nonce != self.nonce:
                 raise HTTPException(401, detail="Nonce does not match")
-
-    def check_lock(self):
-        if not self.lock.acquire(blocking=False):
-            raise HTTPException(status_code=429, detail="some Method is already being executed.")
 
     def get_fn(self, method_name: str):
         if method_name.startswith("__"):
@@ -96,16 +49,9 @@ class MangaShare:
     async def listen(self, translation_params: dict = None):
         app = FastAPI()
 
-        @app.get("/is_locked")
-        async def is_locked():
-            if self.lock.locked():
-                return {"locked": True}
-            return {"locked": False}
-
         @app.post("/simple_execute/{method_name}")
         async def execute_method(request: Request, method_name: str = Path(...)):
             self.check_nonce(request)
-            self.check_lock()
             method = self.get_fn(method_name)
             attr = await load_data(request, method)
             try:
@@ -113,24 +59,10 @@ class MangaShare:
                     result = await method(**attr)
                 else:
                     result = method(**attr)
-                self.lock.release()
                 result_bytes = pickle.dumps(result)
                 return Response(content=result_bytes, media_type="application/octet-stream")
             except Exception as e:
-                self.lock.release()
                 raise HTTPException(status_code=500, detail=str(e))
-
-        @app.post("/execute/{method_name}")
-        async def execute_method(request: Request, method_name: str = Path(...)):
-            self.check_nonce(request)
-            self.check_lock()
-            method = self.get_fn(method_name)
-            attr = await load_data(request, method)
-
-            # streaming response
-            streaming_response = StreamingResponse(self.progress_stream(), media_type="application/octet-stream")
-            asyncio.create_task(self.run_method(method, **attr))
-            return streaming_response
 
         config = uvicorn.Config(app, host=self.host, port=self.port)
         server = uvicorn.Server(config)
